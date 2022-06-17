31e894c1dfe4127deb3d912212efea045f4988f0
unknown <semtehrani@yahoo.com>
2022-06-17 14:58:06 -0700

    bonus part : Added image in serializers.py + defined image in models.py , added image urls to each stone , runs perfectly

--- stones/models.py
+++ stones/models.py
@@ -8,3 +8,7 @@ class Stone(models.Model) :
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits = 8, decimal_places = 2)     # we have define the decimal places
     inventory = models.IntegerField()
+    image = models.CharField(max_length=1000000,default='0000-00-00')
+
+
+    # addede image for bonus part
--- stones/serializers.py
+++ stones/serializers.py
@@ -6,4 +6,7 @@ from .models import Stone
 class StoneSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stone
-        fields = ['id','title','description', 'price', 'inventory']
+        fields = ['id','title','description', 'price', 'inventory', 'image']
+
+
+        # added image for bonus part
