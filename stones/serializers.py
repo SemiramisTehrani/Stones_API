4e8c0938fab76c5b8f207926562ed769f671d379
unknown <semtehrani@yahoo.com>
2022-06-16 21:49:00 -0700

    created serializers.py

---
+++ stones/serializers.py
@@ -0,0 +1,9 @@
+# we made this
+
+from rest_framework import serializers   
+from .models import Stone
+
+class StoneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stone
+        fields = ['id','title','description', 'price', 'inventory']
