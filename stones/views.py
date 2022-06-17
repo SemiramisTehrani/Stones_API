617ed6354665e120ec90b939930094592b31d21d
unknown <semtehrani@yahoo.com>
2022-06-16 22:13:21 -0700

    created postman Stones_API + GET

--- stones/views.py
+++ stones/views.py
@@ -3,10 +3,14 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import StoneSerializer
+from .models import Stone
 
 
 @api_view(['GET'])
 def stones_list(request) :
-
-    return Response('ok')
+    
+        stones = Stone.objects.all()
+        serializer = StoneSerializer(stones, many = True)
+        return Response(serializer.data)
 
